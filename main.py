79e0a189e71a93a61e73ec84f1d6271f6c5b5cf7
BLACK-143 <126906227+NORMALUSER-143@users.noreply.github.com>
2023-03-17 17:49:59 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from os import system as sy
+from fake_email import Email
+
+sy('clear')
+print('Free temp-mail used pip module (fake-email)\nCreate By Afrlyn Shishigami')
+mail = Email().Mail()
+print('\nYour temp-mail : '+ mail['mail'])
+print('Waiting Message...\n')
+while True:
+    mess = Email(mail['session']).inbox()
+    if mess:
+        print('From : '+ mess['from'] +' | '+ mess['name'])
+        print('Subject : '+ mess['topic'])
+        print('Message : '+ mess['message'])
+        break
+Footer
